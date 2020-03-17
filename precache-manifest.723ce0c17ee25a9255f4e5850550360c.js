ad3ebde9d3b7e4f415a73fd31353a7172479b5e4
Kaustav Karmakar <kaustavkarmakar@Kaustavs-MacBook-Air.local>
2020-03-17 15:07:03 +0530

    Updates

---
+++ precache-manifest.723ce0c17ee25a9255f4e5850550360c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d90136f6340275efd04",
+    "url": "/ReactLibaryApp/static/css/main.1f9940ac.chunk.css"
+  },
+  {
+    "revision": "6d90136f6340275efd04",
+    "url": "/ReactLibaryApp/static/js/main.6d90136f.chunk.js"
+  },
+  {
+    "revision": "f26ca547ea1825018188",
+    "url": "/ReactLibaryApp/static/js/1.f26ca547.chunk.js"
+  },
+  {
+    "revision": "195b8d9a8c4a000dae29",
+    "url": "/ReactLibaryApp/static/js/runtime~main.195b8d9a.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/ReactLibaryApp/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/ReactLibaryApp/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/ReactLibaryApp/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "0a73cdfeccacf1aebe1be7980ca1b4c5",
+    "url": "/ReactLibaryApp/index.html"
+  }
+];
